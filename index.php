a6ad8e9523bf029ff8692e835ac7d06144c52e07
Marco D'Amico <marco.mdamico@gmail.com>
2016-03-28 12:16:39 +0200

    first commit Condominio-MS

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+$env = getenv('APP_ENV') ?: 'production';
+
+if($env == "development") 
+{
+  error_reporting(E_ALL | E_STRICT);
+  ini_set('display_errors', true);
+}
+*/
+
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors', true);
+
+/**
+ * This makes our life easier when dealing with paths. Everything is relative
+ * to the application root now.
+ */
+//chdir(dirname(__DIR__));
+
+// Decline static file requests back to the PHP built-in webserver
+if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
+    return false;
+}
+
+// Setup autoloading
+require 'init_autoloader.php';
+
+// Run the application!
+Zend\Mvc\Application::init(require 'config/application.config.php')->run();
